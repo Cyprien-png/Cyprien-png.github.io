d37ed27aee8a65a13005621b42e1838bb77785fa
Cyprien-png <61788240+Cyprien-png@users.noreply.github.com>
2021-10-31 21:19:49 +0100

    trying tailwind
    trying tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [
+    './view/*.php',
+    './view/content/Js/*.js'
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: { 
+
+      fontFamily: {
+      'title' : ['Goldman'],
+      'text' : ['Sawarabi Gothic'],
+     },
+
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+}
